08f5783aa917d21041c599e5b7935ee53b134e63
aymeeeeeen <afifiayman6@gmail.com>
2020-09-24 15:33:12 +0200

    Création de la BD

---
+++ src/main/java/org/sid/gestionstock/dao/LigneVente.java
@@ -0,0 +1,16 @@
+package org.sid.gestionstock.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity @Data @AllArgsConstructor @NoArgsConstructor
+public class LigneVente {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
